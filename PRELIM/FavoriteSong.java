75a82466d9aeb5d28418b04eb4be6f2b4adb4714
ThereseArcenal <148034400+ThereseArcenal@users.noreply.github.com>
2023-10-18 06:27:44 +0100

    Add files via upload

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,21 @@
+/*
+  Name: Therese Andrei C. Arcenal
+  Yr&Sec: BSIT 2B
+  Date: September 13,2023
+  Activity:1 TableAndChairs
+*/
+
+ class FavoriteSong
+ {
+   
+   public static void main(String[] args)
+   {  
+     
+      System.out.println("You know that I can see right through you");
+      System.out.println("I can read your mind");
+      System.out.println("I can read your mind"); 
+      System.out.println("What you want to do?");   
+   
+   }
+ }
+   
